bc97c629b81fb81f535ab656cd8d58f89a5c6b90
Matthew Justice <matthewajustice@gmail.com>
2021-01-27 19:38:29 -0500

    everything done but the add move form. Had lots of stuff going on irl tonight

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import MovieCard from "./MovieCard";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams, useHistory } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 function Movie({ addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  const { push } = useHistory();
 
   const fetchMovie = (id) => {
     axios
@@ -18,6 +19,17 @@ function Movie({ addToSavedList }) {
     addToSavedList(movie);
   };
 
+  const editMovie = () => {
+    push(`/update-movies/${params.id}`);
+  };
+
+  const deleteMovie = () => {
+    axios
+      .delete(`http://localhost:5000/api/movies/${movie.id}`)
+      .then((res) => push('/'))
+      .catch((err) => console.log(err.response));
+  };
+
   useEffect(() => {
     fetchMovie(params.id);
   }, [params.id]);
@@ -27,11 +39,17 @@ function Movie({ addToSavedList }) {
   }
 
   return (
-    <div className="save-wrapper">
+    <div className='save-wrapper'>
       <MovieCard movie={movie} />
 
-      <div className="save-button" onClick={saveMovie}>
+      <div className='save-button' onClick={saveMovie}>
         Save
+      </div>
+      <div className='save-button edit-button' onClick={editMovie}>
+        Edit
+      </div>
+      <div className='save-button delete-button' onClick={deleteMovie}>
+        Delete
       </div>
     </div>
   );
